915490f9bf11ef40b5dd27dfa479315ac7a12cd1
homepodz <info.homepodz@gmail.com>
2025-10-27 12:28:00 -0400

    Integrate Global Delivery section into Index page
    Added Global Delivery section to the Index page.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,26 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import HowItWorksSection from "@/components/HowItWorksSection";
+import GlobalDelivery from "@/sections/GlobalDelivery"; // New section import
 import Footer from "@/components/Footer";
 import SocialProofToast from "@/components/SocialProofToast";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
+      {/* Hero section */}
       <HeroSection />
+
+      {/* Features section */}
       <FeaturesSection />
+
+      {/* How It Works section */}
       <HowItWorksSection />
+
+      {/* Global Delivery map section */}
+      <GlobalDelivery />
+
+      {/* Footer and social proof */}
       <Footer />
       <SocialProofToast />
     </div>
